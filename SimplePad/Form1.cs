c029e129d9b8d00b8b2c8624b8c480b910df8d84
William Harrington <kb0iic@gmail.com>
2017-03-17 14:43:41 -0500

    Add project files.

---
+++ SimplePad/Form1.cs
@@ -0,0 +1,51 @@
+﻿// http://www.instructables.com/id/How-to-create-a-Notepad-in-VS-C/?ALLSTEPS
+
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SimplePad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Show the dialog.
+            DialogResult result = fontDialog1.ShowDialog();
+
+            // See if OK was pressed.
+            if (result == DialogResult.OK)
+            {
+                // Get Font.
+                Font font = fontDialog1.Font;
+                // Set TextBox Properties.
+                this.textBox1.Text = string.Format("Font is: {0}", font.Name);
+                this.textBox1.Font = font;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Text File | *.txt";
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            string name = saveFileDialog1.FileName;
+            File.WriteAllText(name, textBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
